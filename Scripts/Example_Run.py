0222acf542d28e8680f7323b849dceca7b8e5602
IT-gMA <tiendatoan@gmail.com>
2022-04-08 20:27:32 +0800

    options to specify directory to store running results

--- Scripts/Example_Run.py
+++ Scripts/Example_Run.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os.path
+import os
 import argparse
 from IGTD_Functions import min_max_transform, table_to_image
 
@@ -7,19 +8,36 @@ from IGTD_Functions import min_max_transform, table_to_image
 # Retrieve the tabular data file's name in the input argument line
 def parse_opt(known=False):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', type=str, default='Data.txt', help='Tabular data file (saved in ../Data/) to be converted to image')
+    parser.add_argument('--data', type=str, default='Data.txt',
+                        help='Tabular data files name (saved in ../Data/) to be converted to image')
+    parser.add_argument('--result', type=str, default='Results',
+                        help='Directory to save the recent runs')
 
     opt = parser.parse_known_args()[0] if known else parser.parse_args()
     return opt
 
 
 opt = parse_opt(True)
+# Verify whether the input tabular data file exists
 tab_data_path = '../Data/Data.txt'
 if opt.data:
     tab_data_path = '../Data/{}'.format(opt.data)
     if not os.path.exists(tab_data_path):
         raise Exception("The tabular data can't be found")
 
+# Verify the integrity of the Result storing folder
+saved_result_dir = '../Results/'
+if opt.result:
+    saved_result_dir = '../{}/'.format(opt.result)
+    print(f"save in {saved_result_dir}")
+    # Create this new directory if it does not exist yet
+    if not os.path.exists(tab_data_path):
+        print("It doesnt exist")
+        os.makedirs(saved_result_dir, 0)
+    else:
+        # Check whether this directory is empty in case it already exists
+        if os.listdir(saved_result_dir):
+            raise Exception("This directory already has datas in it")
 
 num_row = 3  # Number of pixel rows in image representation
 num_col = 4  # Number of pixel columns in image representation
@@ -28,7 +46,7 @@ num_col = 4  # Number of pixel columns in image representation
 num = num_row * num_col  # Number of features to be included for analysis, which is also the total number of pixels in image representation
 save_image_size = 2  # Size of pictures (in inches) saved during the execution of IGTD algorithm.
 max_step = 10000  # The maximum number of iterations to run the IGTD algorithm, if it does not converge. S_max
-val_step = 100  # The number of iterations for determining algorithm convergence. If the error reduction rate
+val_step = 600  # The number of iterations for determining algorithm convergence. If the error reduction rate
 # is smaller than a pre-set threshold for val_step itertions, the algorithm converges.  S_con
 
 # BATCH NORMALISATION: Import the example data and linearly scale each feature so that its minimum and maximum values are 0 and 1, respectively.
@@ -45,7 +63,7 @@ fea_dist_method = 'Euclidean'  # (1) feature distance
 image_dist_method = 'Euclidean'  # (2) pixel distance
 error = 'abs'  # difference between the feature distance and pixel distance ranking matrices
 # (can either take the absolute or square value)
-result_dir = '../Results/Test_1'  # Where we save the computed difference (results)
+result_dir = '{}Test_1'.format(saved_result_dir)  # Where we save the computed difference (results)
 os.makedirs(name=result_dir, exist_ok=True)
 table_to_image(norm_data, [num_row, num_col], fea_dist_method, image_dist_method, save_image_size,
                max_step, val_step, result_dir, error)  # Using the IGTD's function
@@ -57,7 +75,7 @@ table_to_image(norm_data, [num_row, num_col], fea_dist_method, image_dist_method
 fea_dist_method = 'Pearson'  # Distance (or difference kinda) between features (smaller)
 image_dist_method = 'Manhattan'  # Difference between the matrices (containing features or pixels) (bigger)
 error = 'squared'
-result_dir = '../Results/Test_2'
+result_dir = '{}Test_2'.format(saved_result_dir)
 os.makedirs(name=result_dir, exist_ok=True)
 table_to_image(norm_data, [num_row, num_col], fea_dist_method, image_dist_method, save_image_size,
                max_step, val_step, result_dir, error)
